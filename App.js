4067c7b497c1bca3805c8b8e967d8bedde477706
Can Tam <60790925+zMaax-source@users.noreply.github.com>
2022-01-13 18:01:29 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,63 @@
+let page = 0;
 
-const text = ['Powerfull, Reliable, Flexible']
+const pageSetter = (pageNumber) => {
+  page = pageNumber;
+};
 
-let currentText = '';
-let letter = '';
-let count = 0;
-let index = 0;
+const bindWheel = () => {
+  $(window).bind("mousewheel DOMMouseScroll", scrollPage);
+};
 
+const unBindWheel = () => {
+  $(window).unbind();
+};
 
+$(window).bind("mousewheel DOMMouseScroll", scrollPage);
 
-(function type(){
+function scrollPage(event) {
+  if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+    // scroll up
+  } else {
+    // scroll down
+    if (page == 0) {
+      $(".home").fadeOut(150);
+      pageSetter(1);  
+      unBindWheel();
+      setTimeout(() => {
+        $("#home__section").load("./pages/page2.html", function() {  $("#home__section").hide().fadeIn(150); });
+      }, 160);
+      setTimeout(() => {
+        bindWheel();
+      }, 1000);
+    
+    }else if (page == 1) {
+      $(".page2__home").fadeOut(150);
+      pageSetter(2);
+      unBindWheel();
 
-    if(count === text.length){
-        count = 0;
+      setTimeout(() => {
+        $("#home__section").load("./pages/page3.html", function() { $("#home__section").hide().fadeIn(150) });
+      }, 160);
+      setTimeout(() => {
+        bindWheel();
+      }, 1000);
     }
 
-    currentText = text[count]
-    letter = currentText.slice(0, ++index)
+    else if (page == 2) {
+      $(".page2__home").fadeOut(150);
+      pageSetter(3);
+      unBindWheel();
 
-    document.querySelector('.type').textContent = letter
+      setTimeout(() => {
+        $("#home__section").load("./pages/page4.html", function() { $("#home__section").hide().fadeIn(150) });
+      }, 160);
+      setTimeout(() => {
+        bindWheel();
+      }, 1000);
+    }
+  }
+}
 
-    setTimeout(type, 100)
-}());
+$("#signin").click(function() {
+  window.location.href = "./pages/login/login.html"
+})
